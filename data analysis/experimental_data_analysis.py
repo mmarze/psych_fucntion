f08dd5dbccf3c7587560c58c33107f4f852fafdc
Marcin M <124877129+mmarze@users.noreply.github.com>
2024-03-31 15:54:27 +0200

    Update experimental_data_analysis.py
    Code explenation added

--- data analysis/experimental_data_analysis.py
+++ data analysis/experimental_data_analysis.py
@@ -1,10 +1,10 @@
+# author: Marcin J Marzejon
 import numpy as np
 import pandas as pd
 pd.set_option("display.precision", 10)
 
-# Expreimental parameters - parameters to change
-# file_path - path to the file
-file_path = r"/Users/marcin/Desktop/Wed, Mar 15, 2023_2.25.38 PM.txt"
+# file_path – path to the file
+file_path = r" #your_path# "
 # M - number of brightness levels
 M = 12
 # k - number of queries for each brightness level
@@ -23,16 +23,21 @@ exp_data = exp_data.replace(to_replace='Y', value=1)
 exp_data = exp_data.replace(to_replace='N', value=0)
 
 # result data container
-results = np.zeros((M,4))
-# Calculate mean power level, std(power level) and fraction detected stimuli
+results = np.zeros((M,5))
+
+# Calculate mean power level, std(power level), log10(power level) @pupil plane,
+# log10(std(power level)), and number of detected stimuli
+# trans_coeff – ratio of power levels: @pupil plane and measured by power meter in system
+trans_coeff = 0.6200
 for i in range(0, M):
     results[i, 0] = exp_data.iloc[k*i:k*(i+1), 1].mean()
     results[i, 1] = exp_data.iloc[k*i:k*(i+1), 1].std()
-    results[i, 2] = exp_data.iloc[k*i:k*(i+1), 2].sum()
-    results[i, 3] = exp_data.iloc[k*i:k*(i+1), 2].sum()*10
+    results[i, 2] = np.log10(trans_coeff*results[i, 0])
+    results[i, 3] = np.log10(results[i, 0]/(results[i, 0]-results[i, 1]))
+    results[i, 4] = exp_data.iloc[k*i:k*(i+1), 2].sum()
 # converts results to pd.DataFrame 
 results = pd.DataFrame(results, columns=['mean power [W]', 'power std [W]', 
                                     'No yes answers', 'fraction detected [%]'])
-# save results to file
+# save results to Excel file
 file_path2 = file_path[:-3] + 'xlsx'
 results.to_excel(file_path2)
